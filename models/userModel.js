cec9a5382b09a168d0986afc3c2331d8b7ae4f7f
TalhaJubaerPrantor <talhajubaer3121@gmail.com>
2025-10-20 08:13:01 +0600

    d

---
+++ models/userModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+
+    name: { type: String },
+    email: { type: String },
+    password: { type: String },
+    phone: { type: String },
+    age: { type: Number },
+    address: { type: String },
+    bloodGroup: { type: String },
+    totalDonations: { type: Number, default: 0 },
+    eligibility: { type: Boolean, default: true },
+    points: { type: Number, default: 0 },
+    totalDonations: { type: Number, default: 0 },
+    donationHistory: { type: Array, default: [] },
+    bloodRequests: { type: Array, default: [] },//People who requested me for blood
+    bloodRequested: { type: Array, default: [] }//People I requested for blood
+
+})
+
+
+module.exports =new mongoose.model("User", userSchema); 
